673296d49b98bca453c22f38c65ac90ee43af832
Danilo Silva <danilo.silva@msn.com>
2025-05-10 21:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ CodeSnapshot/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeSnapshot
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CodeSnapshot/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeSnapshot
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
